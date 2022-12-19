5a612a8e4899bce192e200dde96973b2e5264281
Matt Moresco <mmoresco@stripe.com>
2022-12-19 15:27:48 -0500

    Day 1

---
+++ day1.py
@@ -0,0 +1,34 @@
+from util import *
+
+def parse_food(lines):
+    return [[int(meal) for meal in elf.strip().split('\n')] for elf in lines.split('\n\n')]
+
+def most_cals_with_top_n_elves(elves, n=1):
+    return sum(MaxN([sum(elf) for elf in elves], n))
+
+test_input = """1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000"""
+
+# PART 1
+
+assert parse_food(test_input) == [[1000, 2000, 3000], [4000], [5000, 6000], [7000, 8000, 9000], [10000]]
+assert most_cals_with_top_n_elves(parse_food(test_input)) == 24000
+
+elves = parse_food(Input(1).read())
+print("Part 1:", most_cals_with_top_n_elves(elves))
+
+# Part 2
+assert most_cals_with_top_n_elves(parse_food(test_input), 3) == 45000
+print("Part 2:", most_cals_with_top_n_elves(elves, 3))
--- util.py
+++ util.py
@@ -1,7 +1,11 @@
 def Input(day):
-    "Open this day's input file."
+    '''Open this day's input file.'''
     filename = 'input{}.txt'.format(day)
     try:
         return open(filename)
     except FileNotFoundError:
         print("No input file found for day", day)
+
+def MaxN(l, n=1):
+    '''Find top N elements in list'''
+    return sorted(l, reverse=True)[:n]
